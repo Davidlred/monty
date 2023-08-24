a0cea238dc2411716c87211a8197c47aa005a5be
davidlred <davidprincewill20@gmail.com>
2023-08-24 15:23:21 -0700

    check

--- main.c
+++ main.c
@@ -1,22 +1,18 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - monty code interpreter
- * @argc: argument
- * @argv: monty file location
- * Return: 0 on sucess
- */
-
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-
-	char *content = NULL;
+	char *content;
 	FILE *file;
-	char *read_line;
+	size_t size = 0;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
-
 	unsigned int counter = 0;
 
 	if (argc != 2)
@@ -24,7 +20,6 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
 	bus.file = file;
 	if (!file)
@@ -32,17 +27,20 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	while ((read_line = fgets(content, sizeof(char) * 1024, file)) != NULL)
+	while (read_line > 0)
 	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
-		runner(content, &stack, counter, file);
+		if (read_line > 0)
+		{
+			runner(content, &stack, counter, file);
+		}
 		free(content);
 	}
-
 	clear_stack(stack);
 	fclose(file);
-	return (0);
+return (0);
 }
 
